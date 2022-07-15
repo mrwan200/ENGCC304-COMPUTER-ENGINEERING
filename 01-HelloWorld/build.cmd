e7b262e1e027522cc3446f2f837a41d611826f4b
M-307 <unturnedmc1@gmail.com>
2022-07-15 17:10:17 +0700

    Remove build script and exe file

--- 01-HelloWorld/build.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-cls
-echo "Building file"
-
-g++ ./main.cpp -o main.exe
-
-echo "Running..."
-
-.\main.exe
--- 03-Codex/build.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-cls
-echo "Building file"
-
-g++ ./main.c -o main.exe
-
-echo "Running..."
-
-.\main.exe
