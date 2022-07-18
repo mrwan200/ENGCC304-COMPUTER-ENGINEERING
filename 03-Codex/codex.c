53df1bf112db1028c8d7d1e3ce6f3532e5ab04b5
M-307 <unturnedmc1@gmail.com>
2022-07-19 00:17:37 +0700

    Format like codex standard

--- 03-Codex/codex.c
+++ 03-Codex/codex.c
@@ -8,26 +8,24 @@ int main() {
     char star   = '*' ;
 
 
-    printf("--| Show 'Hello World' to Screen %d line(s)\n", N) ;
-    for(i = 0 ; i < N ; i++) {
+    printf( "--| Show 'Hello World' to Screen %d line(s)\n", N ) ;
+    for( i = 0 ; i < N ; i++ ) {
         printf( "[%3d] Hello World\n", i ) ;
     }//end for
     
-    // Set new values and reset i to 0
+    // Set new values and reset i to zero
     N   = 8 ;
     i   = 0 ;
-    printf("\n");
+    printf( "\n" );
 
-    while(i < N){
+    while(i < N) {
         j = 0;
-        while(j < N){
-            if(i < j){
-                printf("%c", star) ; 
-            }//end if
+        while(j < N) {
+            if(i < j) printf( "%c", star );
             j++;
         }//end while
 
-        printf("%s", "\n") ;
+        printf( "%s", "\n" ) ;
         i += 1 ;
     }//end while
 
